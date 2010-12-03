303c60fe9599c1c544e8aa466649c0199ddc8354
Bob Burbach <bob.burbach@gmail.com>
2010-12-03 15:33:52 -0800

    Force compilation of sass stylesheets. If you have an external error (like a missing directory or submodule) they won't update even if you fix it. So for deploying we just force to make sure updates happen.

--- lib/recipes/sass/compile.rb
+++ lib/recipes/sass/compile.rb
@@ -2,9 +2,9 @@ Capistrano::Configuration.instance(:must_exist).load do
   
   namespace :sass do
     
-    desc "Generate the compiled CSS files from Sass"
+    desc "Generate (force) the compiled CSS files from Sass"
     task :update_stylesheets, :roles => [:static] do
-      run "cd #{current_path}; script/runner -e #{rails_env} 'Sass::Plugin.update_stylesheets'"
+      run "cd #{current_path}; script/runner -e #{rails_env} 'Sass::Plugin.options[:always_update] = 1; Sass::Plugin.update_stylesheets'"
     end
     
   end # end namespace
