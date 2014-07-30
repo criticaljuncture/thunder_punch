f7d4947ba9db74bc0708a2595b7a221db73e0a81
Peregrinator <bob.burbach@gmail.com>
2014-07-30 15:25:50 -0700

    Fix bug caused by use of wrong variable

--- lib/thunder_punch/recipes/bundler.rb
+++ lib/thunder_punch/recipes/bundler.rb
@@ -5,7 +5,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Install and lock bundle"
     task :bundle, :roles => bundler_roles do
       _cset(:excluded_gem_file_groups) { [:development, :test] }
-      excluded_groups = gem_file_groups.reject{|e| e == rails_env.to_sym}.each{|g| g.to_s}.join(' ') 
+      excluded_groups = excluded_gem_file_groups.reject{|e| e == rails_env.to_sym}.each{|g| g.to_s}.join(' ') 
       run "cd #{current_path} && bundle install --deployment --without #{excluded_groups}"
     end
   end
