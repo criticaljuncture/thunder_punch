5286522e11c4446351178ce8f5cf8a0ee601f5d9
Peregrinator <bob.burbach@gmail.com>
2015-01-07 10:56:25 -0800

    Fix task to work with old rails (2.x) and new rubies

--- lib/thunder_punch/recipes/sass/compile.rb
+++ lib/thunder_punch/recipes/sass/compile.rb
@@ -5,7 +5,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :sass do
     desc "Generate (force) the compiled CSS files from Sass"
     task :update_stylesheets, :roles => sass_roles do
-      run "cd #{current_path}; script/runner -e #{rails_env} 'Sass::Plugin.options[:always_update] = 1; Sass::Plugin.update_stylesheets'"
+      run "cd #{current_path}; ./script/runner -e #{rails_env} 'Sass::Plugin.options[:always_update] = 1; Sass::Plugin.update_stylesheets'"
     end
   end
 end
