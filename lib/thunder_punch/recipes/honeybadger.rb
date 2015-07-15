5547bf30c2d506ba42eee9248f1ef50898053818
Peregrinator <bob.burbach@gmail.com>
2015-07-15 15:56:21 -0700

    Add a task that allows us to use the env variables to call out to honeybadger cli when deploying

--- lib/thunder_punch/recipes/honeybadger.rb
+++ lib/thunder_punch/recipes/honeybadger.rb
@@ -6,4 +6,17 @@ Capistrano::Configuration.instance(:must_exist).load do
       run "cd #{current_path} && #{rake} honeybadger:deploy RAILS_ENV=#{rails_env} TO=#{branch} USER=#{honeybadger_user} REVISION=#{real_revision} REPO=#{repository}"
     end
   end
+
+  namespace :honeybadger_cli do
+    task :notify_deploy, :roles => honeybadger_roles do
+      api_key = fetch(:honeybadger_api_key_var, "$HONEYBADGER_API_KEY")
+      precommand = fetch(:honeybadger_cli_precommand, "")
+
+      cmds = ["cd #{current_path}"]
+      cmds << precommand if precommand != ""
+      cmds << "bundle exec honeybadger deploy -e #{rails_env} -r #{repository} -s #{real_revision} -u #{honeybadger_user} -k #{api_key}"
+
+      run "#{cmds.join(' && ')}"
+    end
+  end
 end
